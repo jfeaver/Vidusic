d5f54275237fbeda1f1d1dca445f28b4578846b4
Nathan <nathan@nathanfeaver.com>
2012-05-10 16:15:17 -0600

    fresh app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidusic::Application.config.secret_token = '49db26dd0a5900178277ed383d11b844aa84469b4577abf3b285f81fb82517387bcf45e7960282114e9479f24389002275befbf8b0f8cf3d71ebb1eed9b8b48b'
